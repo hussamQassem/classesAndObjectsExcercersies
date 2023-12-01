760e418ffa1bc8bdc4452808642c1740f0e8ceca
hussamQassem <hussamcacem@gmail.com>
2023-12-01 04:11:02 +0000

    hello from class

--- src/main/java/com/mycompany/classesandobjectsexcercersies/ClassesAndObjectsExcercersies.java
+++ src/main/java/com/mycompany/classesandobjectsexcercersies/ClassesAndObjectsExcercersies.java
@@ -14,5 +14,6 @@ public class ClassesAndObjectsExcercersies {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("how are you?");
+        System.out.println("hello from class");
     }
 }
