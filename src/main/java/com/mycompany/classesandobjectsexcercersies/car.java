a74c90fd585d8324500ceca17f2b7a8e825f1004
hussamQassem <hussamcacem@gmail.com>
2023-12-01 04:21:30 +0000

    created car class and properties
    make,modle,color,numWheels

---
+++ src/main/java/com/mycompany/classesandobjectsexcercersies/car.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.classesandobjectsexcercersies;
+
+/**
+ *
+ * @author hussa
+ */
+public class car {
+    String make;
+    String model;
+    String color;
+    int numWhells;
+    
+    
+    
+    
+    
+    
+}
